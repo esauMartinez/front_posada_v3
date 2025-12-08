f20f040297795556661d86910b082fd7a19c9450
esauMartinez <martinez117esau@gmail.com>
2025-12-07 21:03:46 -0600

    se modifico la url del api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // export const url = 'https://api-posada-v3-c2908b9e58f3.herokuapp.com'
-export const url = 'http://localhost:4075'
+export const url = 'https://recawi.com.mx'
 export const urlsocket = 'wss://recawi.com.mx'
 
 export const raffleApiConnection = axios.create({ baseURL: `${url}/api-posada` })
